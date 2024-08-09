c55c9e3efbb32a2a9dcc87545d94bf502f3c1b53
Katy Williamson <k.williamson34@gmail.com>
2024-08-09 14:37:40 -0400

    add intro

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,7 +7,54 @@ export const metadata: Metadata = {
 export default function AboutPage() {
   return (
     <>
-      <h1>About me</h1>
+      <section>
+        <h2>Intro</h2>
+        <p>
+          What gives me purpose in my work? Enablement, efficiency, impact. I
+          have 10+ years of experience as an engineer working across the stack,
+          and 3+ years providing technical leadership and unlocking high
+          performance in delivery teams. I have started teams from scratch,
+          built strong cross-functional partnerships, mentored other engineers,
+          and delivered large features at high velocity and high quality. I am
+          passionate about the user experience. My teammates recognize me for
+          inspiring and upholding high standards in solution design, code
+          quality, and end-to-end ownership.
+        </p>
+        <p>
+          I started my software engineering journey with an Intro to CS class at{" "}
+          <a href="https://compsci.sites.tjhsst.edu/">TJHSST</a>. I&apos;ve
+          interned in a server basement and become briefly addicted to 5-Hour
+          Energy. I fumbled through building an immersive{" "}
+          <a
+            href="https://www.mechdyne.com/av-vr-solutions/blog/direct-view-led-technology-the-roi-of-led-displays/"
+            target="_blank"
+          >
+            VR experience
+          </a>{" "}
+          with Unity! I got my degrees in Computer Science and Computer
+          Engineering at UVA, got my first real job, and built a{" "}
+          <a
+            href="https://medium.com/@miladd3/avoid-using-apache-cordova-if-you-can-6d1654bba5bd"
+            target="_blank"
+          >
+            hybrid app
+          </a>{" "}
+          back when they were cool. Thus I was launched as a web app engineer,
+          evolving over many different projects and teams to become the
+          tech lead I am today.
+        </p>
+        <p>
+          Besides software, I care about gardening and{" "}
+          <a
+            href="https://www.earthday.org/actions/call-for-green-spaces"
+            target="_blank"
+          >
+            green roofs
+          </a>
+          , backpacking in national parks, scuba diving, watercolor painting,
+          Orange Theory Fitness, coffee, and the DMV food scene.
+        </p>
+      </section>
       <section>
         <h2>Projects</h2>
         <ol>
