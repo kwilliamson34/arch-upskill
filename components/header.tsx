4f9772f60b60560914bbea42deaabd1ac33503a2
Katy Williamson <k.williamson34@gmail.com>
2024-08-09 11:15:17 -0400

    mobile layout fix

--- components/header.tsx
+++ components/header.tsx
@@ -14,8 +14,8 @@ const Header = () => {
         href={href}
         className={clsx(
           "inline-block rounded-t-2xl",
-          "text-gray-800 no-underline text-lg font-bold tracking-wide",
-          "mt-2 mr-2 py-2 px-4",
+          "text-gray-800 no-underline font-bold tracking-wide",
+          "mt-2 mr-2 py-2 px-2 md:px-4",
           href == pathname ? "bg-white" : "bg-gray-200"
         )}
       >
