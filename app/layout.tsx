68d12723237bd1bbdb4dd89c47b921f5e40399a6
Katy Williamson <k.williamson34@gmail.com>
2024-08-13 14:47:24 -0400

    format fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,9 +21,23 @@ export default function Layout({ children }) {
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
 
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="favicon_/favicon-16x16.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon_io/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon_io/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="favicon_/favicon-16x16.png"
+        />
         <link rel="manifest" href="/site.webmanifest"></link>
       </head>
 
