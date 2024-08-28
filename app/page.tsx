8edc30d71d09b7dbcf7653ea70c025550ba3f0db
Katy Williamson <k.williamson34@gmail.com>
2024-08-28 14:26:39 -0400

    shift section order

--- app/page.tsx
+++ app/page.tsx
@@ -15,9 +15,25 @@ export default async function HomePage() {
   return (
     <>
       <section className="my-8">
-        <h2 className="container mb-4">Daily Tasks</h2>
+        <h2 className="container mb-4">Learning Goal Calendar</h2>
+        <div className="grid grid-cols-1 lg:grid-cols-5">
+          <div className="hidden h-6 text-center lg:block">M</div>
+          <div className="hidden h-6 text-center lg:block">T</div>
+          <div className="hidden h-6 text-center lg:block">W</div>
+          <div className="hidden h-6 text-center lg:block">Th</div>
+          <div className="hidden h-6 text-center lg:block">F</div>
+          {sortByDate(allPosts).map((post) => (
+            <CalendarItem post={post} key={post.id} className="mb-4 lg:mr-4" />
+          ))}
+          {getUndated(allPosts).map((post) => (
+            <CalendarItem post={post} key={post.id} className="mb-4 lg:mr-4" />
+          ))}
+        </div>
+      </section>
+
+      <section className="my-8">
+        <h2 className="container mb-4">My helpful links</h2>
         <ol className="ml-8 list-decimal">
-          <li>Learning goal</li>
           <li>
             Algorithms and data structures practice:{" "}
             <a href="https://leetcode.com/explore/" target="_blank">
@@ -55,23 +71,6 @@ export default async function HomePage() {
             </a>
           </li>
         </ol>
-      </section>
-
-      <section>
-        <h2 className="container mb-4">Learning Goal Calendar</h2>
-        <div className="grid grid-cols-1 lg:grid-cols-5">
-          <div className="hidden h-6 text-center lg:block">M</div>
-          <div className="hidden h-6 text-center lg:block">T</div>
-          <div className="hidden h-6 text-center lg:block">W</div>
-          <div className="hidden h-6 text-center lg:block">Th</div>
-          <div className="hidden h-6 text-center lg:block">F</div>
-          {sortByDate(allPosts).map((post) => (
-            <CalendarItem post={post} key={post.id} className="mb-4 lg:mr-4" />
-          ))}
-          {getUndated(allPosts).map((post) => (
-            <CalendarItem post={post} key={post.id} className="mb-4 lg:mr-4" />
-          ))}
-        </div>
       </section>
     </>
   );
